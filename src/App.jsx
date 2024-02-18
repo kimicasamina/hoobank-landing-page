b6d7d272dfa20243bc5b50f26f91e05180ca646e
kimi.casamina <kimi.casamina@gmail.com>
2024-02-18 15:15:13 +0800

    add constants, assets, and custom tailwindcss styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import styles from './styles'
+
+import {Navbar, Billing, Business, Button, CardDeal, Clients, CTA, FeedbackCard, Footer, GetStarted, Hero} from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='bg-blue-500 max-w-[800px] mx-auto flex px-12'>
-        <h1 className="text-6xl font-bold underline text-orange-700">
-        Hello world!
-        </h1>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`styles.boxWidth`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`styles.boxWidth`}>
+          Hero
+        </div>
+      </div>
     </div>
   )
 }
